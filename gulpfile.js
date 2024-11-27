97867aae47af50429cd126cf3c34b8a1edf5af85
Leandro Ramos <leandrodeadkiller2018@gmail.com>
2024-11-27 10:35:44 -0300

    Criação do hero

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,11 @@ function styles() {
 
 function images() {
     return gulp.src('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'));
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images);   
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
 }
